97412c7df351897130d63b2ea89f16379223ddf7
Micky Hulse <mickyhulse@gmail.com>
2017-12-06 12:06:55 -0800

    App dir

---
+++ app/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Apache HTTP Server</title>
+	</head>
+	<body>
+		<h3>PHP version: <?=phpversion()?></h3>
+		<p><a href="phpinfo.php">phpinfo</a></p>
+	</body>
+</html>
